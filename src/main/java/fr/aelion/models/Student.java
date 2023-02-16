46f64afa5a51120cbac91b7827c0b742bcec852f
elsa <elsa-david@laposte.net>
2023-02-16 16:14:41 +0100

    Initial commit

---
+++ src/main/java/fr/aelion/models/Student.java
@@ -0,0 +1,51 @@
+package fr.aelion.models;
+
+public class Student {
+    public String lastName;
+    public String firstName;
+    private String email;
+    private String username;
+    private String password;
+    private Boolean isLoggedIn = false;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public Student(String lastName, String firstName, String email) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+
+    public Boolean login(String username, String password) {
+        if (username.equals(this.username) && password.equals(this.password)) {
+            this.isLoggedIn = true;
+            return true;
+        }
+        return false;
+    }
+
+    public void logout() {
+        this.isLoggedIn = false;
+    }
+
+    public Boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+
+
+
+}
+
