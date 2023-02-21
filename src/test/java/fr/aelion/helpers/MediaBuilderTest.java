5901ee7c3deee22215cf74fecd4a1d5ae7f77a96
elsa <elsa-david@laposte.net>
2023-02-21 15:14:04 +0100

    Create MediaBuilder from Interface + test

---
+++ src/test/java/fr/aelion/helpers/MediaBuilderTest.java
@@ -0,0 +1,52 @@
+package fr.aelion.helpers;
+
+import fr.aelion.models.courses.Author;
+import fr.aelion.models.courses.Media;
+import fr.aelion.models.courses.Video;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class MediaBuilderTest {
+    private MediaBuilder mediaBuilder = new MediaBuilder();
+
+    @BeforeEach
+    void setUp() {
+        mediaBuilder.setMediaType("video");
+        mediaBuilder
+                .title("Test")
+                .summary("Youplaboom trop cool")
+                .author(new Author())
+                .duration(5.35F);
+    }
+
+    @Test
+    @DisplayName("Should be an instanceof Video class")
+    void build() {
+        assertTrue(mediaBuilder.build() instanceof Video);
+    }
+
+    @Test
+    @DisplayName("Title shoud be 'Test'")
+    void titleAttributeTest() {
+        Media video = mediaBuilder.build();
+        assertEquals("Test", video.getTitle());
+    }
+
+    @Test
+    @DisplayName("Should have correct attribute values")
+    void attributesTest() {
+        Media video = mediaBuilder.build();
+        Float duration = 5.35F;
+        assertAll(
+                () -> assertEquals("Test", video.getTitle()),
+                () -> assertEquals("Youplaboom trop cool", video.getSummary()),
+                () -> assertEquals(duration, video.getDuration()),
+                () -> assertTrue(video.getAuthor() instanceof Author)
+        );
+    }
+}
