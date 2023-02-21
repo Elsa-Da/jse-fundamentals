9cdfc5f59280ea4833e79d46002471dfbf89b26f
elsa <elsa-david@laposte.net>
2023-02-21 11:01:10 +0100

    Add CourseRun and run course from Main

--- src/main/java/fr/aelion/Main.java
+++ src/main/java/fr/aelion/Main.java
@@ -1,17 +1,13 @@
 package fr.aelion;
 
 
-import fr.aelion.run.PersonRun;
-import fr.aelion.run.StudentRun;
+import fr.aelion.run.CourseRun;
 
 public class Main {
 
     public static void main(String[] args) {
-        PersonRun personRun = new PersonRun();
-        personRun.run();
-
-        StudentRun studentRun = new StudentRun();
-        studentRun.run();
+        CourseRun courseRun = new CourseRun();
+        courseRun.run();
     }
 
 }
---
+++ src/main/java/fr/aelion/run/CourseRun.java
@@ -0,0 +1,37 @@
+package fr.aelion.run;
+
+import fr.aelion.models.courses.Document;
+import fr.aelion.models.courses.Media;
+import fr.aelion.models.courses.Slide;
+import fr.aelion.models.courses.Video;
+import fr.aelion.repositories.courses.Course;
+import fr.aelion.services.courses.DisplayCourse;
+
+public class CourseRun {
+
+    private Course course = new Course();
+    public CourseRun() {
+        this.course.setTitle("SQL");
+        this.makeCourse();
+    }
+
+    public void run() {
+        DisplayCourse displayCourse = new DisplayCourse();
+        displayCourse.setCourse(this.course);
+        displayCourse.display();
+    }
+    private void makeCourse() {
+        Media video = new Video();
+        video.setTitle("Create table");
+
+        Media slide = new Slide();
+        slide.setTitle("Alter table");
+
+        Media document = new Document();
+        document.setTitle("Drop table");
+
+        this.course.addMedia(video);
+        this.course.addMedia(slide);
+        this.course.addMedia(document);
+    }
+}
