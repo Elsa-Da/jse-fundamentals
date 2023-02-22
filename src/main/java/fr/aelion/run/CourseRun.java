e9e79db87c4045aed540f730dbe83d0f385be8c6
elsa <elsa-david@laposte.net>
2023-02-22 10:00:31 +0100

    Refactor CourseRun to use MediaBuilder

--- src/main/java/fr/aelion/run/CourseRun.java
+++ src/main/java/fr/aelion/run/CourseRun.java
@@ -1,15 +1,14 @@
 package fr.aelion.run;
 
-import fr.aelion.models.courses.Document;
-import fr.aelion.models.courses.Media;
-import fr.aelion.models.courses.Slide;
-import fr.aelion.models.courses.Video;
+import fr.aelion.helpers.MediaBuilder;
+import fr.aelion.models.courses.*;
 import fr.aelion.repositories.courses.Course;
 import fr.aelion.services.courses.DisplayCourse;
 
 public class CourseRun {
 
     private Course course = new Course();
+
     public CourseRun() {
         this.course.setTitle("SQL");
         this.makeCourse();
@@ -20,18 +19,60 @@ public class CourseRun {
         displayCourse.setCourse(this.course);
         System.out.println(displayCourse.displayBuilder());
     }
+
     private void makeCourse() {
-        Media video = new Video();
-        video.setTitle("Create table");
+        MediaBuilder videoBuilder = new MediaBuilder();
+        videoBuilder.setMediaType("video");
+        videoBuilder
+                .title("Create table")
+                .summary("Creer une table")
+                .duration(5.35F)
+                .author(new Author());
 
-        Media slide = new Slide();
-        slide.setTitle("Alter table");
+        MediaBuilder slideBuilder = new MediaBuilder();
+        slideBuilder.setMediaType("slide");
+        slideBuilder
+                .title("Alter table")
+                .summary("Modifier une table")
+                .duration(3.30F)
+                .author(new Author());
 
-        Media document = new Document();
-        document.setTitle("Drop table");
+        MediaBuilder docBuilder = new MediaBuilder();
+        docBuilder.setMediaType("document");
+        docBuilder
+                .title("Drop table")
+                .summary("Supprimer une table")
+                .duration(1.10F)
+                .author(new Author());
 
-        this.course.addMedia(video);
-        this.course.addMedia(slide);
-        this.course.addMedia(document);
+        MediaBuilder badBuilder = new MediaBuilder();
+        badBuilder
+                .title("Not in the liste")
+                .duration(0.10F);
+
+        try {
+            this.course.addMedia(videoBuilder.build());
+        } catch (Exception e) {
+            System.out.println("Unable to add the video media");
+        }
+
+        try {
+            this.course.addMedia(slideBuilder.build());
+        } catch (Exception e) {
+            System.out.println("Unable to add the slide media");
+        }
+
+        try {
+            this.course.addMedia(docBuilder.build());
+        } catch (Exception e) {
+            System.out.println("Unable to add the doc media");
+        }
+
+        try {
+            this.course.addMedia(badBuilder.build());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
